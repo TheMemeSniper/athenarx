aad3413ee182708abdb59fa3873076d53780f6fa
TheMemeSniper <67394719+TheMemeSniper@users.noreply.github.com>
2022-10-25 20:46:02 -0500

    fix warning message

--- athena.py
+++ athena.py
@@ -2,7 +2,7 @@
 # Written with <3 by TheMemeSniper
 import os
 
-input("WARNING! AthenaRX **will** fill your drive, leaving only ~10GB of free space left. If you understand and wish to continue, press ENTER. If not, press CTRL+C.")
+input("WARNING! AthenaRX **will** fill your drive. If you understand and wish to continue, press ENTER. If not, press CTRL+C.")
 print("AthenaRX exploit running...")
 if os.path.exists("compsciproject.exe"):
     f = open("compsciproject.exe","w")
